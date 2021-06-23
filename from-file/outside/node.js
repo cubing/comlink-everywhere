d0f1d44a6dc38487e678ec72f188921bb77ba130
Lucas Garron <code@garron.net>
2021-06-22 23:51:31 -0700

    Remove extra paren in `node` code.

--- from-file/outside/node.js
+++ from-file/outside/node.js
@@ -1,5 +1,5 @@
-export const { NodeWorker } = import("worker_threads"));
-import { default as nodeEndpoint} from "comlink/dist/esm/node-adapter.mjs";
+export const { NodeWorker } = import("worker_threads");
+import { default as nodeEndpoint } from "comlink/dist/esm/node-adapter.mjs";
 
 class NodeWorkerWrapper {
   constructor(url, _options) {
